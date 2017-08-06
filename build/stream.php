6ecae64a50c29051d61e12cfcf68a4588e72b771
Vahag Dudukgian <vahag@vcomedia.com>
2017-08-05 23:48:27 -0700

    normalize all date fields

--- build/stream.php
+++ build/stream.php
@@ -19,19 +19,19 @@ $excel = PHPExcel_IOFactory::createReaderForFile($file)
   ->load($file);
 
 //normalize all date fields
-$sheet = $excel->getActiveSheet();
-$MAX_COL = $sheet->getHighestDataColumn();
-$MAX_COL_INDEX = PHPExcel_Cell::columnIndexFromString($MAX_COL);
-for($ii=0;$ii<=$MAX_COL_INDEX;$ii++){
-  $col = PHPExcel_Cell::stringFromColumnIndex($ii);
-  $highestRow = $sheet->getHighestRow();
-  for($row=1; $row <= $highestRow; $row++) {
-    $cellobj=$sheet->getCellByColumnAndRow($col, $row);
-    if(PHPExcel_Shared_Date::isDateTime($cellobj)) {
-      $cellobj->getStyle()->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD);
-    }
-  }
-}
+// $sheet = $excel->getActiveSheet();
+// $MAX_COL = $sheet->getHighestDataColumn();
+// $MAX_COL_INDEX = PHPExcel_Cell::columnIndexFromString($MAX_COL);
+// for($ii=0;$ii<=$MAX_COL_INDEX;$ii++){
+//   $col = PHPExcel_Cell::stringFromColumnIndex($ii);
+//   $highestRow = $sheet->getHighestRow();
+//   for($row=1; $row <= $highestRow; $row++) {
+//     $cellobj=$sheet->getCellByColumnAndRow($col, $row);
+//     if(PHPExcel_Shared_Date::isDateTime($cellobj)) {
+//       $cellobj->getStyle()->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD);
+//     }
+//   }
+// }
 
 PHPExcel_IOFactory::createWriter($excel, 'CSV')
   ->save('php://stdout');
